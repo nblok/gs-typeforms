74c04b93e7ce155fe0f03be343e1bdbd4597a30f
Nathan Blok <ndblok@gmail.com>
2026-04-19 08:42:45 -0700

    Basic fastapi application created

--- api/apps/rest-api/src/rest_api/__init__.py
+++ api/apps/rest-api/src/rest_api/__init__.py
@@ -1,2 +1,14 @@
+from fastapi import FastAPI
+import uvicorn
+
+def create_app() -> FastAPI:
+    app = FastAPI()
+
+    @app.get("/")
+    def root():
+        return {"message": "Hello World"}
+
+    return app
+
 def main() -> None:
-    print("Hello from rest-api!")
+    uvicorn.run(create_app(), host="0.0.0.0", port=8000)
